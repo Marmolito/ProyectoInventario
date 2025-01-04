391da326de223df5a7dd0c8ecdd822f8b9e466f1
Marmolito <fabianmarmolejo07@gmail.com>
2025-01-03 20:14:27 -0500

    Feat: migracion inicial, bodega y configuraciones de fluent api

---
+++ ProyectoInventario.Modelos/Bodega.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre obligatorio")]
+        [MaxLength(60, ErrorMessage = "Nombre maximo de 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripción obligatorio")]
+        [MaxLength(100, ErrorMessage = "Descripción maximo de 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado obligatorio")]
+        public bool Estado { get; set; }
+
+
+    }
+}
--- ProyectoInventario/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoInventario.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
